fb5cb3ca84f2e33c7af71c96cd8eaefe5e8596f2
josepholiverr <josepholiverbiz@gmail.com>
2022-06-02 20:50:24 -0700

    update

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "eclesmvp",
-            dependencies: []),
+            dependencies: [		],
+            path:"Sources"),
         .testTarget(
             name: "eclesmvpTests",
             dependencies: ["eclesmvp"]),
---
+++ Sources/homescreen.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  eclesmvp
+//
+//  Created by joseph on 6/2/22.
+//
+
+import Foundation
+import SwiftUI
+
+@available(iOS 13.0, *)
+struct SwiftUIView: View{
+    var body: some View{
+        Text("Hello")
+    }
+}
+	
