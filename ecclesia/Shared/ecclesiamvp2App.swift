d597657b776db286d5bf194a2bab582039641e45
josepholiverr <josepholiverbiz@gmail.com>
2022-06-02 21:00:52 -0700

    update

---
+++ ecclesia/Shared/ecclesiamvp2App.swift
@@ -0,0 +1,17 @@
+//
+//  ecclesiamvp2App.swift
+//  Shared
+//
+//  Created by joseph on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ecclesiamvp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
